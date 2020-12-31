e173bb70174bd63a5c0499dc1691a0ec75820142
nithyakgeethanand <67506979+nithyakgeethanand@users.noreply.github.com>
2020-12-31 22:07:12 +0530

    added more unit test

--- AirlineTicketBookingSystem/src/test/com/cdac/TestHomeController.java
+++ AirlineTicketBookingSystem/src/test/com/cdac/TestHomeController.java
@@ -5,11 +5,15 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 
 import com.cdac.FlightInfo;
 import com.cdac.FlightInfoModel;
 import com.cdac.HomeController;
+import com.cdac.Login;
+import com.cdac.LoginModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +27,10 @@ import static org.mockito.Mockito.*;
 public class TestHomeController {
 	@Mock
 	FlightInfoModel fm;
+	
+	@Mock
+	LoginModel lm;
+	
 	
 	@InjectMocks
 	HomeController homeController;
@@ -69,5 +77,35 @@ public class TestHomeController {
 		 Assert.assertEquals("sorry", actualResult); 
 	}
 	 
-	
+	 @Test
+	 public void testRegisterForm() {
+		 Model m = new ExtendedModelMap();
+		 
+		 String actualResult = homeController.registerform(m);
+		 Assert.assertEquals("register", actualResult);
+	 }
+	 
+	 @Test
+	 public void testregisterValues() {
+		 Login login = new Login();
+		 
+		 String actualResult = homeController.registerValues(login);
+		 Assert.assertEquals("login", actualResult);
+	 }
+	 
+	 @Test
+	 public void testLoginNavForm() {
+		 Login login = new Login();
+		 
+		 String actualResult = homeController.loginnavform(login);
+		 Assert.assertEquals("login", actualResult);
+	 }
+	 
+	 @Test
+	 public void testAdminAddValues() {
+		 FlightInfo flightinfo = new FlightInfo();
+		 
+		 String actualResult = homeController.adminAddValues(flightinfo);
+		 Assert.assertEquals("adminadddetails", actualResult);
+	 }	
 }
